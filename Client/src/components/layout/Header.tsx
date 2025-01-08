eab9cffe567079698a6002dcf7eb796a9a599e68
B Water <brennw47@gmail.com>
2025-01-08 12:14:00 -0600

    feat(style): Added style imports to the top of index.css to remove warnings.
    feat(style): Added style imports to the top of index.css to remove warnings.

--- Client/src/components/layout/Header.tsx
+++ Client/src/components/layout/Header.tsx
@@ -1,32 +1,34 @@
 import { Link } from "react-router-dom";
+import { useMetaMask } from "../../hooks/useMetaMask";
+import LoginButton from "../common/Login";
 import "../../styles/components/layout/Header.css";
 
 const Header = () => {
+  const { account } = useMetaMask();
+
   return (
     <header className="header">
-      <div className="container">
+      <div className="header-content">
         <nav className="header-nav">
           <div className="header-left">
-            <Link to="/" className="header-logo">
-              {/* Add your logo here */}
-              <span className="logo-text">JYD Token</span>
+            <Link to="/" className="logo">
+              JYD Token
             </Link>
-          </div>
-
-          <div className="header-center">
-            <Link to="/trade" className="nav-link">
-              Trade
-            </Link>
-            <Link to="/pool" className="nav-link">
-              Pool
-            </Link>
-            <Link to="/explore" className="nav-link">
-              Explore
-            </Link>
+            <div className="nav-links">
+              <Link to="/trade" className="nav-link">
+                Trade
+              </Link>
+              <Link to="/pool" className="nav-link">
+                Pool
+              </Link>
+              <Link to="/explore" className="nav-link">
+                Explore
+              </Link>
+            </div>
           </div>
 
           <div className="header-right">
-            <button className="connect-button">Connect Wallet</button>
+            <LoginButton account={account} onLogin={() => {}} />
           </div>
         </nav>
       </div>
